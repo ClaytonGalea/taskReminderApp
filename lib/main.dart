c5d50a5306c75ad7e90f5f6ebf93f96664ecf729
Clayton Galea <claytongalea78@outlook.com>
2025-03-12 21:18:23 +0100

    Added the notification and timezone imports inside main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,9 @@
 import 'package:flutter/material.dart';
 import 'models/task.dart';
 import 'screens/task_screen.dart';
+import 'package:flutter_local_notifications/flutter_local_notifications.dart';
+import 'package:timezone/data/latest.dart' as tz;
+import 'package:timezone/timezone.dart' as tz;
 
 void main() {
   runApp(const TaskReminderApp());
